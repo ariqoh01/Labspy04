042b37b30866d9a577f5fd8727f8711f5a96fc65
ariqoh zulaika zuhrah <zuhrahzulaika@gmail.com>
2021-11-28 15:18:05 +0700

    menambahkan file

---
+++ # Akses List.py
@@ -0,0 +1,16 @@
+# Akses List
+## Menampilkan element ke-3
+bunga = ['mawar', 'melati', 'tulip', 'kamboja', 'laveder', 'teratai']
+print()
+print("List :", bunga)
+print()
+print('Buah dalam element ke-3 adalah', bunga[3])
+print()
+
+## Ambil nilai element ke-2 sampai ke-4
+print(bunga[2:4])
+print()
+
+## Ambil element terakhir
+print(bunga[-1])
+print()
---
+++ Ubah element list.py
@@ -0,0 +1,14 @@
+ # Ubah element list
+## ubah elemen ke-4 dengan nilai lainnya
+bunga = ['mawar', 'melati', 'tulip', 'kamboja', 'levender', 'teratai']
+print()
+print("List sebelum di ubah :", bunga)
+print()
+bunga[4] = 'kembang sepatu'
+print("List sesudah di ubah :", bunga)
+print()
+
+## ubah elemen ke 4 sampai dengan elemen terakhir
+bunga[4:] = ["alamanda", "lily"]
+print("Ubah element ke-4 hingga akhir :", bunga)
+print()
